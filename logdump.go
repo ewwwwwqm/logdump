4444f90994bc0c4a0e39b84e2de237745af4fd78
ewm <epicmaxim@gmail.com>
2017-01-16 16:52:34 +0200

    update for godoc

--- logdump.go
+++ logdump.go
@@ -1,3 +1,4 @@
+// Package for multi-level logging.
 package logdump
 
 import (
@@ -7,19 +8,30 @@ import (
 	"io"
 )
 
+// LogsOutput represents an active logging objects
+// that generates lines of output to an io.Writer.
 type LogsOutput struct {
-	File []FileOutput
+	File []FileOutput // contains log type and output writer
 }
+
+// FileOutput contains log level type and io.Writer for output.
+// Each logging operation makes a single call to 
+// the Writer's Write method.
 type FileOutput struct {
-	Logtype string
-	Logoutput io.Writer
+	Logtype string // contains log level type
+	Logoutput io.Writer // contains io.Writer for output
 }
 
 var logsOutput LogsOutput
 
+// Inits log structure and assigns output.
+// FileOutput.Logoutput points to io.Writer
+// depending on the specified level.
+// After init log output sets to default os.Stderr.
 func Init(logstruct map[string]interface{}) {
 	for k, v := range logstruct {
-		f, err := os.OpenFile(fmt.Sprintf("%v", v), os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+		f, err := os.OpenFile(fmt.Sprintf("%v", v), 
+			os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 		if err != nil {
 		    log.Fatalf("Error opening log file: %v", err)
 		}
@@ -31,6 +43,8 @@ func Init(logstruct map[string]interface{}) {
 	log.SetOutput(os.Stderr)
 }
 
+// Write dumps message to the specified level.
+// On error prints message to default os.Stderr.
 func Write(logtype string, message string) {
 	success := false
 	for _, v := range logsOutput.File {
